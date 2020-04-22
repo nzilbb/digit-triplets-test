0101d27b3aa9751eb229a9eb7c42170bb82e0c86
Robert Fromont <robert@fromont.net.nz>
2020-04-22 17:12:07 -0300

    Basic admin app structure.

--- client/admin/src/app/app.module.ts
+++ client/admin/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FieldsComponent } from './fields/fields.component';
+import { UsersComponent } from './users/users.component';
+import { TextsComponent } from './texts/texts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FieldsComponent,
+    UsersComponent,
+    TextsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      AppRoutingModule,
+      RouterModule.forRoot([
+          { path: '', component: FieldsComponent },
+          { path: 'users', component: UsersComponent },
+          { path: 'texts', component: TextsComponent },
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
