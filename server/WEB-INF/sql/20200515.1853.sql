8b9b2fff380b8c93a29426032b19f1652bcc13c2
Robert Fromont <robert@fromont.net.nz>
2020-05-18 11:28:53 -0300

    Add email settings.

---
+++ server/WEB-INF/sql/20200515.1853.sql
@@ -0,0 +1,5 @@
+INSERT INTO attribute (attribute, value, description, type) VALUES
+('EmailHost','','Email server host name', 'string'),
+('EmailUser','','Email user name', 'string'),
+('EmailPassword','','Email password', 'string'),
+('EmailAddress','','Emails sent from', 'string');
